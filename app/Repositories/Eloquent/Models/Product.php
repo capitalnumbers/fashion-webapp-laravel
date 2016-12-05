ab03698bd5279f31a7a2e1b28051e95af66d0db4
Ismail <ismail@capitalnumbers.com>
2016-12-05 09:59:40 +0530

    removing fillable property

--- app/Repositories/Eloquent/Models/Product.php
+++ app/Repositories/Eloquent/Models/Product.php
@@ -21,12 +21,4 @@ class Product extends Model
      * @var array
      */
     protected $guarded = ['currency_code', 'created_at', 'updated_at'];
-
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = ['product_name', 'product_description', 'product_price', 'in_stock', 'product_seller'];
 }
